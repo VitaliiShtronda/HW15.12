12822f534b6986ad50a0776a6952afb3c65000eb
VitaliiShtronda <v.shtronda@ukr.net>
2024-03-26 07:16:36 +0200

    Initial commit

---
+++ src/main/java/org/example/entities/Client.java
@@ -0,0 +1,18 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+
+    private Long id;
+    @Column(name = "name", length = 200)
+    private String name;
+}
---
+++ src/main/java/org/example/entities/hibernate/HibernateUtils.java
@@ -0,0 +1,41 @@
+package org.example.entities.hibernate;
+
+import lombok.Getter;
+import org.example.entities.Client;
+import org.example.entities.Planet;
+
+import org.example.entities.postgresdb.PostgresDBInitService;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Configures Hibernates as singleton
+ */
+public class HibernateUtils {
+
+    private static final HibernateUtils INSTANCE = new HibernateUtils();
+    private SessionFactory sessionFactory;
+
+    private HibernateUtils() {
+        //initialize db
+        new PostgresDBInitService();
+
+        //configure hibernate
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Client.class)
+                .addAnnotatedClass(Planet.class)
+                .buildSessionFactory();
+    }
+
+    public static HibernateUtils getInstance(){
+        return INSTANCE;
+    }
+
+    public SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+    public void close(){
+        sessionFactory.close();
+    }
+}
